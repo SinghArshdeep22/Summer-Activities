b918c8831934fbc983f53d5e1efd8d1c3be10cad
Singh Arshdeep <arshsngh05@gmail.com>
2024-08-18 12:31:29 +0200

    Adding the First Vue Project

---
+++ First Vue Project/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {computed, ref, watch} from 'vue'
+import Title from './components/Title.vue'; 
+
+const testo = ref("")
+function changeText() {
+  testo.value = 'Changed Text';
+};
+const comp = computed(() => "--> " + testo.value + " <--");
+const wat = watch(
+  () => testo.value, 
+  (newValue, oldValue) => {
+    console.log("newValue: ", newValue);
+    console.log("oldValue: ", oldValue);
+  });
+</script>
+
+<template>
+<div>{{ testo }}</div>
+<div>{{ comp }}</div>
+<input type="text" v-model="testo">
+<button @click="changeText">Click me!</button>
+<TitleVue :title="'Hello World'"></TitleVue>
+</template>
